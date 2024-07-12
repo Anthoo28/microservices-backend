0ae5cfb5707c6d8c0f95c7e3ff7cc87adf0cbfec
Anthoo28 <zarek1404@gmail.com>
2024-07-12 00:02:22 -0500

    agregando servicio rest de usuario fix

--- microservicio-usuarios/src/main/java/backend/microserviciousuarios/Dao/UsuarioDao.java
+++ microservicio-usuarios/src/main/java/backend/microserviciousuarios/Dao/UsuarioDao.java
@@ -1,13 +1,14 @@
 package backend.microserviciousuarios.Dao;
 
 import backend.microserviciousuarios.Model.Usuario;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path="usuarios")
-public interface UsuarioDao  extends PagingAndSortingRepository<Usuario, Long> {
+public interface UsuarioDao extends CrudRepository<Usuario, Long> {
 
      @RestResource(path = "buscar-username")
      public Usuario findByUsername(String username);
+
 }
